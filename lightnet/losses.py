1e120106490fae5013167581cfad4bc5b03fe76a
scrge-ai <56807509+scrge-ai@users.noreply.github.com>
2022-08-09 18:21:15 -0700

    Update losses.py

--- lightnet/losses.py
+++ lightnet/losses.py
@@ -4,12 +4,15 @@ import numpy as np
 class MSE(core.Loss):
     def __init__(self):
         def meanSquaredError(y, yhat):
-            return 1/2 * (np.mean(y-yhat))**2
+            s = 0
+            for i in range(len(y)):
+                s += (y[i]-yhat[i])**2
+            
+            return 1/(2*len(y)) * s
 
         def meanSquaredErrorGrad(y, yhat, i): #only for 1d output for now
-            n = y.size
             #print(y, yhat, i)
-            return 1/n * (y[i]-yhat[i])
+            return 1/len(y) * (y[i]-yhat[i])
 
         super(MSE, self).__init__(meanSquaredError, meanSquaredErrorGrad)
 
