15fda734bb310d470d8c9109e302e08ce89c27aa
scrge-ai <56807509+scrge-ai@users.noreply.github.com>
2022-08-14 15:07:44 -0700

    added saving/loading models

---
+++ core.py
@@ -0,0 +1,113 @@
+import numpy as np
+import os
+
+"""
+def load_layer(path, name):
+    #for i in range(len(name_list)):
+    layer_config = open("path/" + name + "_config", "r")
+    layer_type = layer_config.readline()
+    if(layer_type == "dense"):
+        weights = np.load("path/" + name + "_weights.npy")
+        biases = np.load("path/" + name + "_biases.npy")
+"""
+class Activation:
+    def __init__(self, func, grad):
+        self.func = func
+        self.grad = grad
+
+class FeedForward:
+    def __init__(self):
+        pass
+
+    def call(self, inlayer):
+        pass
+
+    def backprop(self, memo, a, z): #TODO
+        pass
+
+    def apply_grads(self, grads):
+        pass
+
+    def save_layer(self):
+        pass
+
+"""
+class FeedForward: #two completely connected layers; can be thought of as a nn with 0 hidden layers
+    def __init__(self, weights, biases, activation): #TODO: add more customization (for flatten layers, etc.)
+        self.weights = weights #output layer shape is determined by weights
+        self.biases = biases
+        self.activation = activation #class activation
+
+    def call(self, inlayer):
+        #return self.activation.func(inlayer @ self.weights + self.biases), inlayer @ self.weights + self.biases
+        return self.activation.func(np.dot(inlayer, self.weights)+self.biases), np.dot(inlayer, self.weights)+self.biases
+"""
+
+class Sequential: #links many FeedForwards into a completely connected ANN
+    def __init__(self, varsArr, name="sequential"):
+        self.varsArr = varsArr #weights, biases, that kind of stuff
+        self.trainableArr = [False for i in range(len(varsArr))]
+        self.name = name
+
+    def call(self, inlayer, training=False):
+        ret = []
+        ret_noactivation = []
+        noactivation = np.copy(inlayer)
+        for i in range(len(self.varsArr)):
+            ret.append(inlayer)
+            ret_noactivation.append(noactivation)
+            inlayer, noactivation = self.varsArr[i].call(inlayer)
+        ret.append(inlayer)
+        ret_noactivation.append(noactivation)
+
+        if(not training):
+            return inlayer
+        else:
+            return ret, ret_noactivation
+
+    def save(self):
+        path = self.name
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        for i in range(len(self.varsArr)):
+            self.varsArr[i].save_layer(path)
+            
+class Loss: #includes both gradient and loss function
+    def __init__(self, loss, grad):
+        self.loss = loss
+        self.grad = grad #gradient with respect to output layer (output layer as input)
+
+    def getLoss(self, model, x, y): #x = logit, y = label
+        return self.loss(model.call(x), y)
+
+    def getGrad(self, y, yhat, i):
+        return self.grad(y, yhat, i)
+
+class AutoGrad:
+    def __init__(self, loss):
+        self.loss = loss #class Loss type (sorry for potential confusion)
+    
+    def getGrad(self, model):
+        pass
+
+class Optimizer:
+    def __init__(self, loss, gradcalc, lr=0.1):
+        self.loss = loss
+        self.gradcalc = gradcalc
+        self.lr = lr
+    
+    def step(self):
+        pass
+
+#not using this for now o7
+class TrainSession: #optimizer
+    def __init__(self, model, loss, autograd):
+        self.model = model
+        self.loss = loss
+        self.autograd = autograd #autograd calculates gradient step for each train step
+
+    def trainBatch(self, logits, labels):
+        for i in range(len(logits)):
+            logit = logits[i]
+            label = labels[i]
+        
---
+++ grads.py
@@ -0,0 +1,32 @@
+#TODO: add batch training
+#gradient calculators
+from lightnet import core, layers, activations, losses
+import numpy as np
+
+#chain rule: dy/dx = dy/du * du/dx
+#y is loss, x is weight -> u is 
+
+#backprop step for a hidden layer of distance one from output layer: dC/dw = dC/da2 * da2/dw = dC/da2 * do(a1w)/a1w * da1w/dw
+#= dC/da2 * da2/dw * d[o(x)]/dx * a1
+#only one nn call needed for backprop
+
+
+class SchotasticGrad(core.AutoGrad):
+    def __init__(self, loss):
+        super(SchotasticGrad, self).__init__(loss)
+
+    def getGrad(self, model, x, y):
+        out, out_noactivation = model.call(x, training=True)
+        
+        param_grads = [None for i in range(len(model.varsArr))]
+        memo = np.zeros((out[-1].shape))
+
+        for i in range(len(out[-1])):
+            memo[i] = self.loss.getGrad(out[-1], y, i)
+
+        for i in range(len(model.varsArr)-1, -1, -1):
+            #print(i)
+            memo, param_grads[i] = model.varsArr[i].backprop(memo, out_noactivation[i], out[i], out_noactivation[i+1], out[i+1])
+
+        return param_grads
+
